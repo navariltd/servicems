e105aee76e234c138131e01e5285780dcbc45f68
symonmuchemi <simonkenya846@gmail.com>
2025-12-02 18:07:56 +0200

    feat: Update hooks.py to include fixtures and improve formatting
    - Added a fixtures definition for Custom Field with filters for the servicems module.
    - Reformatted the user_data_fields and website_route_rules sections for improved readability.

--- servicems/hooks.py
+++ servicems/hooks.py
@@ -11,6 +11,8 @@ app_color = "grey"
 app_email = "info@aakvatech.com"
 app_license = "MIT"
 
+fixtures = [{"doctype": "Custom Field", "filters": [["module", "=", "servicems"]]}]
+
 # Includes in <head>
 # ------------------
 
@@ -46,7 +48,7 @@ app_include_js = "/assets/servicems/js/shortcut.js"
 
 # website user home page (by Role)
 # role_home_page = {
-#	"Role": "home_page"
+# 	"Role": "home_page"
 # }
 
 # Generators
@@ -96,7 +98,7 @@ app_include_js = "/assets/servicems/js/shortcut.js"
 # 		"on_update": "method",
 # 		"on_cancel": "method",
 # 		"on_trash": "method"
-#	}
+# 	}
 # }
 
 # Scheduled Tasks
@@ -148,24 +150,22 @@ app_include_js = "/assets/servicems/js/shortcut.js"
 # --------------------
 
 user_data_fields = [
-	{
-		"doctype": "{doctype_1}",
-		"filter_by": "{filter_by}",
-		"redact_fields": ["{field_1}", "{field_2}"],
-		"partial": 1,
-	},
-	{
-		"doctype": "{doctype_2}",
-		"filter_by": "{filter_by}",
-		"partial": 1,
-	},
-	{
-		"doctype": "{doctype_3}",
-		"strict": False,
-	},
-	{
-		"doctype": "{doctype_4}"
-	}
+    {
+        "doctype": "{doctype_1}",
+        "filter_by": "{filter_by}",
+        "redact_fields": ["{field_1}", "{field_2}"],
+        "partial": 1,
+    },
+    {
+        "doctype": "{doctype_2}",
+        "filter_by": "{filter_by}",
+        "partial": 1,
+    },
+    {
+        "doctype": "{doctype_3}",
+        "strict": False,
+    },
+    {"doctype": "{doctype_4}"},
 ]
 
 # Authentication and authorization
@@ -177,5 +177,5 @@ user_data_fields = [
 
 
 website_route_rules = [
-	{'from_route': '/bookings/<path:app_path>', 'to_route': 'bookings'},
+    {"from_route": "/bookings/<path:app_path>", "to_route": "bookings"},
 ]
