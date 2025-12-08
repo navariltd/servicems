76c7e852d74303ee40614b9a8f917a68a48eec33
symonmuchemi <simonkenya846@gmail.com>
2025-12-08 11:25:02 +0200

    feat: Add completion date to Service Job Card
    - Updated the complete_service_job_card function to set the completion_date field when all tasks are completed.
    - Modified the service_job_card.json to include the completion_date field in the field order and defined its properties as a read-only Datetime field, making it visible in filters and list views.

--- servicems/service_management/controllers/service_job_card.py
+++ servicems/service_management/controllers/service_job_card.py
@@ -1,11 +1,13 @@
 import frappe
 from frappe.desk.form.assign_to import add
+from frappe.utils import now
 
 
 def complete_service_job_card(doc, method=None):
     incomplete_tasks = [task.task_name for task in doc.tasks if task.completed == 0]
 
     if not incomplete_tasks:
+        doc.completion_date = now()
         doc.status = "Completed"
 
 
